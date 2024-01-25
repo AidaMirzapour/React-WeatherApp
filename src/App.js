53722dda9b901e063fcc642b46139b2fbbb48f5a
Itsaida <113546067+Itsaida@users.noreply.github.com>
2024-01-25 16:39:19 +0330

    update bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Weather from "./Weather";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather />
+      </div>
+    </div>
+  );
 }
 
 export default App;
